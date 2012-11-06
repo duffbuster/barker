88c0eeb14f5fbbcba32799c3c18ac48531d1e024
Colin <colin@fishbrains.com>
2012-11-05 19:33:58 -0500

    Barker Init
    Created Baker package for use in Eclipse

---
+++ barker/src/barkerMain.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Colin Mackey and David Justis
+ * 
+ *
+ */
+public class barkerMain {
+
+	/**
+	 * Constructor
+	 */
+	public barkerMain() {
+		
+		super();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
